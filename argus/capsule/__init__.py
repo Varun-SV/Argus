7c77de4bcdc02af22ef7f465a31f3fa0c12ba860
Varun S V <99396752+Varun-SV@users.noreply.github.com>
2026-08-10 21:39:17 +0530

    feat: export multi-os Capsule contracts

--- argus/capsule/__init__.py
+++ argus/capsule/__init__.py
@@ -4,20 +4,24 @@ from argus.capsule.base import (
     CapsuleError,
     CapsuleHandle,
     CapsuleProvider,
+    CapsuleProviderCapabilities,
     CapsuleRequest,
     CapsuleSettings,
     FailureCapsule,
 )
 from argus.capsule.guest import CapsuleGuestError, GuestAdapterProxy, GuestAgentClient
+from argus.capsule.libvirt import LibvirtProvider
 
 __all__ = [
     "CapsuleError",
     "CapsuleGuestError",
     "CapsuleHandle",
     "CapsuleProvider",
+    "CapsuleProviderCapabilities",
     "CapsuleRequest",
     "CapsuleSettings",
     "FailureCapsule",
     "GuestAdapterProxy",
     "GuestAgentClient",
+    "LibvirtProvider",
 ]
